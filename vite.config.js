7bf7c11c6fa2b0cce61764e9393e6da5b159f7ee
s.elkharraz <saif.elkharraz@seven-app.net>
2023-03-07 10:23:16 +0100

    fix config base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/rest-countries-api/",
+  base: "/countries-rest-api/",
   plugins: [react()],
 });
